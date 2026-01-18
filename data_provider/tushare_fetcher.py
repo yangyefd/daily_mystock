e0ddde938d8c1c6e293c44ad6311311dce2aad78
tuoxing <tuoxing@example.com>
2026-01-18 21:29:07 +0800

    fix

--- data_provider/tushare_fetcher.py
+++ data_provider/tushare_fetcher.py
@@ -52,7 +52,7 @@ class TushareFetcher(BaseFetcher):
     """
     
     name = "TushareFetcher"
-    priority = 2
+    priority = 3
     
     def __init__(self, rate_limit_per_minute: int = 80):
         """
